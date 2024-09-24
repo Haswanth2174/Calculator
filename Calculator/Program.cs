3f0c8e94f7a5af516f1bb8589596a2655c0555e6
Haswanth Reddy Mekala <Mekala-HR@ulster.ac.uk>
2024-09-24 15:52:50 +0100

    jkj

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//collecting the first number from the user
+Console.WriteLine("Enter the First Number");
+int firstNumber=Convert.ToInt32 (Console.ReadLine());
+//collecting the second number from the user
+Console.WriteLine("Enter the Second Number");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+//performing the calculation
+int result=firstNumber+secondNumber;
+Console.WriteLine("The sum of the first {0} and second{1} number is {2}",firstNumber, secondNumber, result);
